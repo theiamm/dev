11fc3184cfbfcb2dba675cfd1ff8680499a600e5
Asselya <kalbayevaasselya@gmail.com>
2023-11-29 09:40:56 +0600

    tests

--- myapp/models.py
+++ myapp/models.py
@@ -1,12 +1,13 @@
 # myapp/models.py
 
+# myapp/models.py
 from django.db import models
 
-class Post(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField()
+class Animal(models.Model):
+    name = models.CharField(max_length=100)
+    sound = models.CharField(max_length=100)
 
-    def __str__(self):
-        return self.title
+    def speak(self):
+        return f'The {self.name} says "{self.sound}"'
 
 
--- myapp/tests.py
+++ myapp/tests.py
@@ -1,22 +1,16 @@
-# myapp/tests.py
-
 from django.test import TestCase
-from .models import Post
+from myapp.models import Animal
 
-class PostModelTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        # Создаем тестовые данные, которые будут использоваться во всех методах тестирования
-        Post.objects.create(title='Test Title', content='Test Content')
 
-    def test_title_content(self):
-        post = Post.objects.get(id=1)
-        expected_title = f'{post.title}'
-        expected_content = f'{post.content}'
-        self.assertEqual(expected_title, 'Test Title')
-        self.assertEqual(expected_content, 'Test Content')
+class AnimalTestCase(TestCase):
+    def setUp(self):
+        Animal.objects.create(name="lion", sound="roar")
+        Animal.objects.create(name="cat", sound="meow")
 
-    def test_str_method(self):
-        post = Post.objects.get(id=1)
-        self.assertEqual(str(post), 'Test Title')
+    def test_animals_can_speak(self):
+        """Animals that can speak are correctly identified"""
+        lion = Animal.objects.get(name="lion")
+        cat = Animal.objects.get(name="cat")
+        self.assertEqual(lion.speak(), 'The lion says "roar"')
+        self.assertEqual(cat.speak(), 'The cat says "meow"')
 
